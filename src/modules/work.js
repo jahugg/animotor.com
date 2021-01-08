abe8e8fb0381ad6cd4faff42f7df98c602d938f0
jahugg <jan@huggenberg.ch>
2021-01-08 16:46:01 +0100

    added responsive images

--- src/modules/work.js
+++ src/modules/work.js
@@ -1,4 +1,5 @@
 import projects from './../media/work/*/*.*';
+import projectMedia from './../media/work/*/*/*.*';
 import * as helpers from './helpers.js';
 import Swiper, { Pagination, Lazy } from 'swiper';
 
@@ -15,15 +16,13 @@ export function render() {
     threshold: 1,
   };
 
-  let observer = new IntersectionObserver(handleProjects, options);
+  let observer = new IntersectionObserver(lazyLoadProjects, options);
 
-  function handleProjects(entries, observer) {
+  function lazyLoadProjects(entries, observer) {
     entries.forEach((entry) => {
       // fill viewport with projects
       // and append new projects when intersection conditions are met
       if (entry.intersectionRatio === 1) {
-        console.log(entry.target.id, 'loaded');
-
         //stop observing this object
         observer.unobserve(entry.target);
 
@@ -36,6 +35,7 @@ export function render() {
     });
   }
 
+  // create basic html structure
   let main = document.getElementById('main');
   let slideshowsContainer = document.createElement('div');
   slideshowsContainer.classList.add('slideshows-container');
@@ -86,18 +86,42 @@ export function render() {
       slidesWrapper.appendChild(slide);
 
       // add media item using swipers lazy load functionality
-      if (fileType === 'jpg' || fileType === 'png' || fileType === 'gif' || fileType === 'jpeg' || fileType === 'webp') {
-        let media = document.createElement('img');
-        media.setAttribute('data-src', filePath);
-        media.setAttribute('data-srcset', filePath + ' 2x');
-        media.alt = `Image of project ${project[0]}`;
-        media.classList.add('slideshow__slide__media', 'swiper-lazy');
-        slide.appendChild(media);
+      // handling standard images
+      if (fileType === 'jpg' || fileType === 'jpeg' || fileType === 'png' || fileType === 'webp') {
+        let picture = document.createElement('picture');
+        picture.classList.add('slideshow__slide__picture');
+        slide.appendChild(picture);
+
+        let fileSet = createFilenameSet(filePath);
+
+        picture.innerHTML = `
+        <source data-srcset="${fileSet.webp[400]} 1x, ${fileSet.webp[800]} 2x" type="image/webp" media="(max-width: 500px)">
+        <source data-srcset="${fileSet.min[400]} 1x, ${fileSet.min[800]} 2x" media="(max-width: 500px)">
+        <source data-srcset="${fileSet.webp[600]} 1x, ${fileSet.webp[1200]} 2x" type="image/webp" media="(max-width: 700px)">
+        <source data-srcset="${fileSet.min[600]} 1x, ${fileSet.min[1200]} 2x" media="(max-width: 700px)">
+        <source data-srcset="${fileSet.webp[1200]}" type="image/webp">
+        <source data-srcset="${fileSet.min[1200]}">
+        <img data-src="${filePath}" alt="Image of project ${project[0]}" class="swiper-lazy">
+        `;
 
         let preloader = document.createElement('div');
         preloader.classList.add('swiper-lazy-preloader');
         slide.appendChild(preloader);
+
+      // handling GIF files
+      } else if (fileType === 'gif') {
+        let media = document.createElement('img');
+        media.setAttribute('data-src', filePath);
+        media.alt = `Image of project ${project[0]}`;
+        media.classList.add('slideshow__slide__media', 'swiper-lazy');
+        slide.appendChild(media);
+
+      // unknown file type
+      } else {
+        console.error('invalid filetype');
       }
+
+      observer.observe(slideshow);
     }
 
     // create pagination
@@ -128,15 +152,52 @@ export function render() {
     // adding custom slideshow events
     /* register intersection observer after first image has 
       been loaded to lazy load next slideshows */
-    swiper.on('lazyImageReady', (event) => {
-      // add observer after first image has been loaded
-      if (!event.el.getAttribute('data-firstimg')) {
-        event.el.setAttribute('data-firstimg', 'loaded');
-        observer.observe(slideshow);
-      }
-    });
+    // swiper.on('lazyImageReady', (swiper, slideEl, imageEl) => {
+    //   console.info('lazy image ready, slide:', slideEl.getAttribute('data-swiper-slide-index'));
+    //   // add observer to slideshow after first image has been loaded
+    //   if (!swiper.el.getAttribute('data-firstimg')) {
+    //     swiper.el.setAttribute('data-firstimg', 'loaded');
+    //     observer.observe(slideshow);
+    //     console.info('observer added for', swiper.el.id);
+    //   }
+    // });
 
     // jump to next slide on click
     if (multipleImages) swiper.on('click', () => swiper.slideNext());
+
+    // create filenames and fetch filepaths based on the root file name
+    // used for responsive images <picture> tag
+    function createFilenameSet(rootFileName) {
+      // extract root name
+      let re = new RegExp('[^.]*');
+      let rootName = re.exec(rootFileName)[0];
+      rootName = rootName.replace(/^\//g, '');
+
+      // create fileset object
+      let fileSet = {
+        min: {},
+        webp: {},
+      };
+
+      // collect filepaths for filenames
+      const sizes = [400, 600, 800, 1200];
+
+      // add jpg and png image paths
+      for (let size of sizes) {
+        let file = projectMedia[projectName].minified[rootName + '-' + size];
+        let fileType = Object.keys(file)[0];
+        let filePath = file[fileType];
+        fileSet.min[size] = filePath;
+      }
+      // add webp image paths
+      for (let size of sizes) {
+        let file = projectMedia[projectName].webp[rootName + '-' + size];
+        let fileType = Object.keys(file)[0];
+        let filePath = file[fileType];
+        fileSet.webp[size] = filePath;
+      }
+
+      return fileSet;
+    }
   }
 }
