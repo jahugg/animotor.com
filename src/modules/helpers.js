59f450149431c65ca66df6d9a54940ec8094dd28
jan@huggenberg.ch <jan@huggenberg.ch>
2021-05-12 15:54:11 +0200

    updated swiper

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -13,8 +13,7 @@ export function throttledEvent(listener, delay) {
 }
 
 export function map(num, in_min, in_max, out_min, out_max) {
-  // taken from
-  // (https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers/23202637)
+  // taken from https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers/23202637
   return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
